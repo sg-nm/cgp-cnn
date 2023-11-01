ad5a9c867011a1805a5696c16925c6de662c12e4
NagallaDeepak <nagalla.deepak@gmail.com>
2023-11-02 00:25:26 +0530

    fix MaxPooling2D

--- cnn_model.py
+++ cnn_model.py
@@ -87,7 +87,7 @@ class CGP2CNN(chainer.Chain):
         i = 1
         for name, in1, in2 in self.cgp:
             if name == 'pool_max':
-                links += [('_'+name+'_'+str(i), F.MaxPooling2D(self.pool_size, self.pool_size, 0, False))]
+                links += [('_'+name+'_'+str(i), F.max_pooling_2d(self.pool_size, self.pool_size, 0, False))]
             elif name == 'pool_ave':
                 links += [('_'+name+'_'+str(i), F.average_pooling_2d(self.pool_size, self.pool_size, 0, False))]
             elif name == 'concat':
